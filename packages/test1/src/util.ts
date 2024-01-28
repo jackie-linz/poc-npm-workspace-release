3213d7c8c6aa9ce699368379891c1a6e08cf2cbe
Jackie Weng <jweng@linz.govt.nz>
2024-01-29 10:31:52 +1300

    feat: test1 with playwright and rollup

---
+++ packages/test1/src/util.ts
@@ -0,0 +1,8 @@
+import { expect } from "@playwright/test";
+
+export const helper1 = async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+};
